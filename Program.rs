518bafaa8504bf9fe4e92f6335b348500b16c9b9
GigaCodeGojo <111521642+GigaCodeGojo@users.noreply.github.com>
2023-09-28 09:16:28 +0530

    Main Code File

--- Program.rs
+++ Program.rs
@@ -1,11 +1,16 @@
+// This is a Rust program designed to remove cache and temporary files from a specified directory.
+// It provides a simple and efficient way to clean up unnecessary files in a given directory.
+
 //Imports
 use std::fs;
 use std::path::Path;
 
 //main function
 fn main() {
+	// Remember to exercise caution when using this program, especially in system directories, as it will permanently delete files.
 	//Defiine the directory path you want to delete/clean
 	let directory_path = "C:\\Windows\\Temp";
+	//Make sure to use correct fiile path or you can use any othe folder to remove files
 	
 	//Attempt to list all files in the directory
 	match fs::read_dir(&directory_path) {
